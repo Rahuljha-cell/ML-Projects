5761423503ab14532032fe63fbfcd61dbe556f55
Rahul Jha <rjha5516@gmail.com>
2025-06-12 19:17:51 +0530

    EDA and Problems statement

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 def error_message_detail(error, error_details:sys):
     _,_,exc_tb=error_details.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -16,3 +17,5 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
+    
